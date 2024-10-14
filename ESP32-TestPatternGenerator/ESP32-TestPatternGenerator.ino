ca8c079f304e43dbfb37367082652874eebf08a0
nmur <null@gmail.com>
2024-10-15 01:10:48 +1100

    made the video format toggle on button press

--- ESP32-TestPatternGenerator/ESP32-TestPatternGenerator.ino
+++ ESP32-TestPatternGenerator/ESP32-TestPatternGenerator.ino
@@ -13,28 +13,30 @@
 
 #include "src/patterns/colorBars.h"
 
-const int REGION_PARAM_ADDR = 0;  // EEPROM address to store parameter choice
-const int EEPROM_SIZE = 4;
-int regionValue = 0;
+const int VIDEO_FORMAT_PARAM_ADDR = 0;  // EEPROM address to store parameter choice
+const int VIDEO_FORMAT_PARAM_SIZE = 4;
+int videoFormatValue = CompositeColorOutput::NTSC;
 
 CompositeGraphics graphics(CompositeColorOutput::XRES, CompositeColorOutput::YRES);
 CompositeColorOutput composite(CompositeColorOutput::NTSC);
 
 Image<CompositeGraphics> colorBars0(colorBars::xres, colorBars::yres, colorBars::pixels);
 
-// Pin where the button is connected
-const int buttonPin = 2;
+const int videoFormatPin = 2;
+int buttonState = HIGH;      // Current state of the button
+int lastButtonState = HIGH;  // Previous state of the button
+unsigned long lastDebounceTime = 0;  // The last time the button state changed
+unsigned long debounceDelay = 50;    // Debounce time in milliseconds
 
 void setup() {
 
-  EEPROM.begin(EEPROM_SIZE);
-  EEPROM.get(REGION_PARAM_ADDR, regionValue);
+  EEPROM.begin(VIDEO_FORMAT_PARAM_SIZE);
+  EEPROM.get(VIDEO_FORMAT_PARAM_ADDR, videoFormatValue);
   
-  //Choose which parameter to use based on the stored value
-  if (regionValue == 1) {
-    composite = CompositeColorOutput(CompositeColorOutput::PAL);
-  } else {
+  if (videoFormatValue == 1) {
     composite = CompositeColorOutput(CompositeColorOutput::NTSC);
+  } else {
+    composite = CompositeColorOutput(CompositeColorOutput::PAL);
   }
 
   esp_pm_lock_handle_t powerManagementLock;
@@ -44,8 +46,7 @@ void setup() {
   composite.init();
   graphics.init();
 
-  // Initialize the button pin as input with the internal pull-up resistor enabled
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(videoFormatPin, INPUT_PULLUP);
 }
 
 void drawImage() {
@@ -60,15 +61,31 @@ void loop() {
   drawImage();
   composite.sendFrameHalfResolution(&graphics.frame);
 
-  delay(2000);
-
-  if (regionValue == 0) {
-    EEPROM.put(REGION_PARAM_ADDR, 1);  // Switch to PAL
-  } else {
-    EEPROM.put(REGION_PARAM_ADDR, 0);  // Switch to NTSC
+  int reading = digitalRead(videoFormatPin);  // Read the state of the button
+  
+  // Check if the button state has changed
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();  // Record the time the button state changed
   }
-    
-  // Commit changes to make sure they're saved
-  EEPROM.commit();
-  esp_restart();  // Reset the ESP32
+
+  // Only change the button state if the debounce time has passed
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // If the button state is LOW, it means the button is pressed
+      if (buttonState == LOW) {
+        if (videoFormatValue == 0) {
+          EEPROM.put(VIDEO_FORMAT_PARAM_ADDR, CompositeColorOutput::NTSC); 
+        } else {
+          EEPROM.put(VIDEO_FORMAT_PARAM_ADDR, CompositeColorOutput::PAL);  
+        }
+        EEPROM.commit();
+        esp_restart();
+      }
+    }
+  }
+
+  lastButtonState = reading;  // Save the current state as the last state for the next loop
 }
+
