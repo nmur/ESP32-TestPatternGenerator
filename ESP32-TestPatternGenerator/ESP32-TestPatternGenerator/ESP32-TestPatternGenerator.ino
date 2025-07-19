9a77c343fded2af70ff3788eb22b40fef57308c6
nmur <null@gmail.com>
2025-07-19 15:29:12 +1000

    battery level display

--- ESP32-TestPatternGenerator/ESP32-TestPatternGenerator/ESP32-TestPatternGenerator.ino
+++ ESP32-TestPatternGenerator/ESP32-TestPatternGenerator/ESP32-TestPatternGenerator.ino
@@ -16,29 +16,23 @@
 
 #include "M5StickCPlus2.h"
 
-void draw_function(LovyanGFX* gfx) {
-    int x      = rand() % gfx->width();
-    int y      = rand() % gfx->height();
-    int r      = (gfx->width() >> 4) + 2;
-    uint16_t c = rand();
-    gfx->fillRect(x - r, y - r, r * 2, r * 2, c);
-}
-
 void setup() {
     auto cfg = M5.config();
     StickCP2.begin(cfg);
-    int textsize = StickCP2.Display.height() / 60;
-    if (textsize == 0) {
-        textsize = 1;
-    }
-    StickCP2.Display.setTextSize(textsize);
+    StickCP2.Display.setRotation(1);
+    StickCP2.Display.setTextColor(GREEN);
+    StickCP2.Display.setTextDatum(middle_center);
+    StickCP2.Display.setTextFont(&fonts::Orbitron_Light_24);
+    StickCP2.Display.setTextSize(1);
 }
 
 void loop() {
-    int x      = rand() % StickCP2.Display.width();
-    int y      = rand() % StickCP2.Display.height();
-    int r      = (StickCP2.Display.width() >> 4) + 2;
-    uint16_t c = rand();
-    StickCP2.Display.fillCircle(x, y, r, c);
-    draw_function(&StickCP2.Display);
+    StickCP2.Display.clear();
+    int vol = StickCP2.Power.getBatteryVoltage();
+    int lev = StickCP2.Power.getBatteryLevel();
+    StickCP2.Display.setCursor(10, 30);
+    StickCP2.Display.printf("VOL: %dmv", vol);
+    StickCP2.Display.setCursor(10, 50);
+    StickCP2.Display.printf("BAT: %dmv", lev);
+    delay(1000);
 }
